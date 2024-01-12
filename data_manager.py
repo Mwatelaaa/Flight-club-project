725ea68428b327cebd773df6fba331b87a5bd60e
Kelvin Mwatela <143689255+Mwatelaaa@users.noreply.github.com>
2024-01-12 23:50:26 +0300

    Add files via upload

---
+++ data_manager.py
@@ -0,0 +1,54 @@
+import requests
+import os
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/4a9970e5f2f6255101467876712a2580/flightDeals/prices"
+SHEETY_USERS_ENDPOINT = "https://api.sheety.co/4a9970e5f2f6255101467876712a2580/flightDeals/users"
+BEARER = os.environ["BEARER"]
+
+
+class DataManager:
+
+    def __init__(self):
+        self.destination_data = {}
+        self.customer_data = {}
+
+    def get_destination_data(self):
+        headers = {
+            "Authorization": f"Bearer {BEARER}",
+            "Content-Type": "application/json",
+        }
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT, headers=headers)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            headers = {
+                "Authorization": f"Bearer {BEARER}",
+                "Content-Type": "application/json",
+            }
+
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data,
+                headers=headers
+            )
+            print(response.text)
+
+    def get_customer_emails(self):
+        headers = {
+            "Authorization": f"Bearer {BEARER}",
+            "Content-Type": "application/json",
+        }
+
+        response = requests.get(url=SHEETY_USERS_ENDPOINT, headers=headers)
+        data = response.json()
+        self.customer_data = data["users"]
+        return self.customer_data
